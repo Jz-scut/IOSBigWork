244d643a336cc188ace2d16b436858c1b1c92e0a
Just different road <65995944+Jz-scut@users.noreply.github.com>
2020-12-11 11:30:08 +0800

    Add files via upload

---
+++ BigWork/BigWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BigWork
+//
+//  Created by 金子 on 2020/12/11.
+//  Copyright © 2020 金子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
